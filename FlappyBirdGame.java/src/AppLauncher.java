89857d9cc2f270bb15d9c6bc7fa764075306ccaf
NikhilSiwach555 <nikhilsiwach555@gmail.com>
2024-12-12 05:22:02 +0530

    Add files via upload

---
+++ FlappyBirdGame.java/src/AppLauncher.java
@@ -0,0 +1,24 @@
+public class AppLauncher{
+    public static void main(String[] args) {
+        AppFrame aFrame = new AppFrame();
+    }
+}
+// background: starting 0,0
+//             width  144
+//             heigth 255
+// upperPipe: starting 56,323
+//             width  26
+//             heigth 160
+// lowerPipe: starting 84,323
+//             width  26
+//             heigth 160
+// bird1: starting 87,491
+//             width  17
+//             heigth 12
+// bird2: starting 115,355
+//             width  17
+//             heigth 12
+// gameOver: starting 395,58
+//             width  96
+//             heigth 21
+//
---
+++ FlappyBirdGame.java/src/Player1.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.net.URL;
+import java.awt.image.RasterFormatException;
+
+public class Player1 {
+    BufferedImage[] birdImages = new BufferedImage[3];
+    BufferedImage completeImage;
+    BufferedImage backgroundImage;
+
+    public Player1() {
+        try {
+            completeImage = ImageIO.read(getClass().getResource("/flappybirdss.png"));
+            loadBirdImages();
+        } catch (IOException e) {
+            System.err.println("Failed to load the complete bird image.");
+            e.printStackTrace();
+        }
+    }
+
+    private void loadBirdImages() {
+        try {
+            BufferedImage birdImage1 = completeImage.getSubimage(87, 491, 17, 12);
+            BufferedImage birdImage2 = completeImage.getSubimage(115, 355, 17, 12);
+            BufferedImage birdImage3 = birdImage2;
+            backgroundImage = completeImage.getSubimage(0, 0, 144, 255);
+
+            birdImages[0] = birdImage1;
+            birdImages[1] = birdImage2;
+            birdImages[2] = birdImage3;
+        } catch (RasterFormatException e) {
+            System.err.println("Error processing subimages. Check the coordinates and dimensions.");
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            System.err.println("Invalid arguments passed to getSubimage. Ensure correct coordinates and dimensions.");
+            e.printStackTrace();
+        }
+    }
+
+    public BufferedImage[] getBirdImage() {
+        return birdImages;
+    }
+
+    public int getBirdHeight() {
+        return birdImages[0].getHeight();
+    }
+
+    public int getBirdWidth() {
+        return birdImages[0].getWidth();
+    }
+
+    public void paintImage(Graphics pen, BufferedImage birdImage, int x, int y) {
+        pen.drawImage(backgroundImage, 0, 0, 600, 700, null);
+        pen.drawImage(birdImage, x, y, 25, 25, null);
+    }
+}
